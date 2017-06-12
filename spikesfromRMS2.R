8e0b5ec0bf635f6ea0462489d9d92f6f156eb83d
Weihua Wang <wangzichengfu@gmail.com>
2017-06-12 17:34:41 +0200

    Create spikesfromRMS2.R
    2nd V

--- spikesfromRMS2.R
+++ spikesfromRMS2.R
@@ -1,20 +1,15 @@
 
-
 # This function aims to locate the spikes from the RMS column and return the spikes' indices if exist.
+# It plots out the RMS vs time before-and-after applying spikes removed method if flag equals "ON".
+# When flag equals to "OFF", it facilitates the concentration checking plot.
 # E.g. "Butane05_d05_1605081338_0.csv" have abnormal RMS values. Some RMS values equal 10, however, 10s seem 
 # do not record any concentration, thus do not affect my previous plotting.
-# 2017-06-08, 1st built, Wang Weihua
+# 2017-06-08, 1st built, 2017-06-12, 2nd modified, Wang Weihua
 
-spikesfromRMS2 <- function(filename,k=8,mon=5) {
+spikesfromRMS2 <- function(filename,flag="ON",width=10,k=8,mon=5) {
   
   source("./dirpath.R")
   source("./SpikeFilter.R")
-  
-  # # for debugging purpose 
-  # filename = "Butane05_d05_1605081518_0"; k=8; mon=5; ch="T"  
-  # filename = "Butane05_d05_1605081420_0"; k=8; mon=5; ch="T"
-  # filename = "Butane05_d05_1605081338_0"; k=8; mon=5; ch="T"
-  # filename = "butadiene_d05_1605081518_0"; k=8; mon=5; ch="T"
   
   dirpath <- dirpath(mon,k,"gps")
   options(digits=9)                      # extend default digits in numeric value
@@ -41,17 +36,28 @@ spikesfromRMS2 <- function(filename,k=8,mon=5) {
   }
     
   dat <- dat[-1, ]
-  width=4
+  
   rr <- SpikeFilter(dat$RMS,width)  # HampelFilter will filter out some small peaks that are not outliers.
-  plot(dat$TimeFromStart, dat$RMS, type='l', col='blue')
-  lines(dat$TimeFromStart, rr[[1]], col='red', type='l')
-  ind <- rr[[2]]
-  ind
-  
-  return(ind)
-  
+  if(flag=="ON"){
+    plot(dat$TimeFromStart, dat$RMS, xlab='TimeFromStart', ylab='RMS', main='RMS checking plot', type='l', col='blue')
+    lines(dat$TimeFromStart, rr[[1]], col='red', type='l')
+    ind <- rr[[2]]
+    ind
+    return(ind)
+  } else if(flag=="OFF"){
+    ind <- rr[[2]]
+    ind
+    return(ind)
+  }
+
 }
 
 # Testing code
-#spikesfromRMS2("Butane05_d05_1605081420_0",k=8,mon=5,ch="T")
-
+# spikesfromRMS2("Butane05_d05_1605081420_0")
+# spikesfromRMS2('Butane05_d05_1605081420_0',"OFF")
+# spikesfromRMS2('Butane05_d05_1605081338_0',"ON")
+# spikesfromRMS2('Butane05_d05_1605081338_0',"OFF")
+# spikesfromRMS2('Butane05_d05_1605081518_0')
+# spikesfromRMS2('Butane05_d05_1605081518_0',"OFF")
+# spikesfromRMS2('butadiene_d05_1605081518_0')
+# spikesfromRMS2('butadiene_d05_1605081518_0',"OFF")
