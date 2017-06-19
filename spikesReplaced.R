3c5d54dd9358786e0cf90b25c6a5d76416ebd09c
Weihua Wang <wangzichengfu@gmail.com>
2017-06-19 16:11:15 +0200

    Update spikesReplaced.R
    use median

--- spikesReplaced.R
+++ spikesReplaced.R
@@ -4,10 +4,11 @@ spikesReplaced <- function(datColumn, indexVec) {
   # return the replaced column-wised data variable.
   # datColumn: the column-wised data variable to be processed.
   # indexVec: the index vector containing the spikes the indices of the spikes.
-  # 2017-06-01, 1st built, Wang Weihua
+  # 2017-06-01, 1st built, 2017-06-19, 2nd modified (mean to median), Wang Weihua
   Vec = c()
-  for (idx in indexVec) { 
-    datColumn[idx] <- mean( c(datColumn[idx-1],datColumn[idx-2],datColumn[idx+1],datColumn[idx+2]) )
+  for (idx in indexVec) {
+    
+    datColumn[idx] <- median( c(datColumn[idx-2],datColumn[idx-3],datColumn[idx-4],datColumn[idx+2],datColumn[idx+3],datColumn[idx+4]) )
     Vec <- c(Vec,datColumn[idx])
   }
   Vec
